ba3b16b45bab22ea81fb7b3fd3c0b449a6e9d50b
Rosie Lucas <maddierose717@gmail.com>
2025-09-28 20:55:01 -0400

    help display added

--- app/calculator/__init__.py
+++ app/calculator/__init__.py
@@ -3,3 +3,27 @@ import readline
 from typing import List
 from app.calculator import Calculation, CalculationFactory
 
+def display_help() -> None:
+    help_message = """
+Calculator REPL Help
+--------------------
+Usage:
+    <operation> <number1> <number2>
+    - Supported operations:
+        add
+        subtract
+        multiply
+        divide
+
+Special Commands:
+    help
+    history
+    exit
+
+Examples:
+    add 10 5
+    subtract 15.5 3.2
+    multiply 7 8
+    divide 20 4
+"""
+    print(help_message)
